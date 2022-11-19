3197ae8d812b1e30a4be438bf3cfaefd98f45f97
davitasryant <davit.asryan.y@tumo.org>
2022-11-19 18:08:47 +0400

    Matrxiy qceci index

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var messages = [];
 
 app.use(express.static("programming3"));
 
@@ -19,6 +18,69 @@ app.get("/", function (req, res) {
 });
 
 server.listen(3000);
+
+
+let matrix = generate(80, 100, 15, 5, 30, 20)
+
+
+let grassArr = [];
+let grassEatArr = [];
+let predatorArr = [];
+let mulBoostArr = [];
+let virusArr = [];
+
+
+function generate(matLen, gr, grEat, pred, mB,virus) {
+    let matrix = []
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = []
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0
+        }
+    }
+
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 1
+        }
+    }
+
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 2
+        }
+    }
+
+    for (let i = 0; i < pred; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 3
+        }
+    }
+
+    for (let i = 0; i < mB; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 4
+        }
+    }
+    for (let i = 0; i < virus; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 5
+        }
+    }
+
+
+    return matrix
+}
 
 function game() {
     for (let i in grassArr) {
@@ -39,6 +101,6 @@ function game() {
         virusArr[i].move()
     }
 }
-
+setInterval(game,1000)
 
 
